f674aa66b7a7f81d26ec6d526bfed27dd4d00aea
Mats Bergstroem <hackingtrom@gmail.com>
2018-04-24 11:56:00 +0200

    Change simple client to use the REST template

--- src/RestClient.java
+++ src/RestClient.java
@@ -1,18 +1,15 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.URL;
+
+import org.springframework.web.client.RestTemplate;
 
 public class RestClient {
 
 	public static void main(String[] args) throws IOException {
 
-		URL url = new URL("http://localhost:8080/mywebapp/customer/CS03939");
-		InputStream is = url.openStream();
-		InputStreamReader isr = new InputStreamReader(is);
-		BufferedReader br = new BufferedReader(isr);
-		System.out.println(br.readLine());
+
+		RestTemplate template = new RestTemplate();
+		String response = template.getForObject("http://localhost:8080/mywebapp/customer/CS03939", String.class);
+		System.out.println(response);
 	}
 
 }
