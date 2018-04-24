d7dadeee37861d5ebdb531e3b6dc7adfbdec8fad
Mats Bergstroem <hackingtrom@gmail.com>
2018-04-24 13:42:20 +0200

    Use jaxb converter to store xml-data in an object
    Important to put the @SmlRootElement annotation on the class
    definition that should hold the converted data.

---
+++ src/Customer.java
@@ -0,0 +1,30 @@
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class Customer {
+
+	private String companyName;
+	private String customerId;
+	private String notes;
+	
+	public String getCompanyName() {
+		return companyName;
+	}
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+	public String getCustomerId() {
+		return customerId;
+	}
+	public void setCustomerId(String customerId) {
+		this.customerId = customerId;
+	}
+	public String getNotes() {
+		return notes;
+	}
+	public void setNotes(String notes) {
+		this.notes = notes;
+	}
+	
+	
+}
--- src/RestClient.java
+++ src/RestClient.java
@@ -6,10 +6,15 @@ public class RestClient {
 
 	public static void main(String[] args) throws IOException {
 
-
 		RestTemplate template = new RestTemplate();
 		String response = template.getForObject("http://localhost:8080/mywebapp/customer/CS03939", String.class);
 		System.out.println(response);
-	}
+		System.out.println();
 
+		Customer customer = template.getForObject("http://localhost:8080/mywebapp/customer/CS03939", Customer.class);
+
+		System.out.println("Customer : " + customer.getCompanyName() + " (id: " + customer.getCustomerId()
+				+ ")\nNotes: " + customer.getNotes());
+
+	}
 }
